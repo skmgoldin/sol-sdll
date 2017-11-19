8f81e888a83b9806f7f968d6c5baf193198ad6d3
Mike Goldin <mike.goldin@consensys.net>
2017-11-19 14:57:31 -0500

    Enforce linting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
-const SDLL = artifacts.require("./SDLL.sol");
+/* global artifacts */
+
+const SDLL = artifacts.require('./SDLL.sol');
 const DLL = artifacts.require('dll/DLL.sol');
 const AttributeStore = artifacts.require('attrstore/AttributeStore.sol');
 
-module.exports = function(deployer) {
-    deployer.deploy(DLL);
-    deployer.deploy(AttributeStore);
+module.exports = function (deployer) {
+  deployer.deploy(DLL);
+  deployer.deploy(AttributeStore);
 
-    deployer.link(DLL, SDLL);
-    deployer.link(AttributeStore, SDLL);
+  deployer.link(DLL, SDLL);
+  deployer.link(AttributeStore, SDLL);
 
-    deployer.deploy(SDLL);
+  deployer.deploy(SDLL);
 };
